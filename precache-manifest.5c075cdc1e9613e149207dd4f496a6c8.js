d0a96bdcc3647e76e8f981065b73ec2d8bf48d6f
Kirankumar Gonti <kirankumar225rk@gmail.com>
2020-04-09 19:50:34 +0530

    Updates

---
+++ precache-manifest.5c075cdc1e9613e149207dd4f496a6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dae32267efec5a690e4ab49973bacc6",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "dd1c5b8ef36f982389c0",
+    "url": "/covid19-tracker/static/css/main.e86cf9eb.chunk.css"
+  },
+  {
+    "revision": "ed7648e88b965b8934b9",
+    "url": "/covid19-tracker/static/js/2.7d7d5dec.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19-tracker/static/js/2.7d7d5dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd1c5b8ef36f982389c0",
+    "url": "/covid19-tracker/static/js/main.4d429c27.chunk.js"
+  },
+  {
+    "revision": "dbae980a6809b37e52bb",
+    "url": "/covid19-tracker/static/js/runtime-main.56e53f11.js"
+  }
+]);
